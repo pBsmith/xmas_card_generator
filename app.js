2f5f17fa7f4fed59274028661dd7178b786fd16e
Parker Smith <pbsmith@rollins.edu>
2018-12-18 16:55:30 -0500

    Add pages

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 8080;
+
+var path = require('path');
+
+app.use(express.static('views'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
+
+app.get('/', function(req, res) {
+    res.render('account_page.html');
+});
+
+app.get('/', function(req, res) {
+    res.render('send_email.html')
+})
+
+app.post('/send-email', function(req, res) {
+    
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'xmascardgen@gmail.com',
+            pass: 'Networks!sC00l'
+        }
+    });
+    
+    var mailOptions = {
+        from: '"The Christmas Card Generator" <xmascardgen@gmail.com>',
+        to: req.body.to,
+        subject: req.body.subject,
+        text: req.body.body,
+        attachments: [ {
+            fileName: "Christmas_Card.png",
+            path: __dirname + "/views/images/Christmas_Card.png"    
+        }]
+    };
+    
+    transporter.sendMail(mailOptions, (error, info) => {
+        
+        if (error) {
+            return console.log(error);
+        } else {
+            console.log(info);
+        }
+        
+    })
+    
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
